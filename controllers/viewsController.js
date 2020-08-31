f300c25362162a4b986d05fe5207c748347d4f85
Ellon Solomon <ellon.solomon@gmail.com>
2020-08-31 03:42:11 +0300

    Improved stripe with messages implementation

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,14 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.locals.alert =
+      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
@@ -55,11 +63,10 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   const tourIDs = bookings.map((el) => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-  res.status(200).render('overview', { 
+  res.status(200).render('overview', {
     title: 'My Tours',
-    tours
-  
-  })
+    tours,
+  });
 });
 
 exports.updateUserdata = catchAsync(async (req, res, next) => {
